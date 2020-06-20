bbfe912bafaa5f5cd546483e61794e4273448b0a
Charlie Wright <charlie@cwgw.co>
2020-06-20 10:02:52 -0700

    wip

---
+++ packages/studio/src/components/previews/iframe.js
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const assembleProjectUrl = ({ displayed, options }) => {
+  const slug = displayed.content.main.slug;
+  const previewURL = options.previewURL;
+  if (!slug || !previewURL) {
+    console.warn("Missing slug or previewURL", { slug, previewURL });
+    return "";
+  }
+  if (slug.current === "home") {
+    return previewURL;
+  }
+
+  return `${previewURL}/${slug.current}`;
+};
+
+class IframePreview extends React.PureComponent {
+  static propTypes = {
+    document: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  };
+
+  static defaultProps = {
+    document: null,
+  };
+
+  render() {
+    console.log({ props: this.props });
+    const { options } = this.props;
+    const { displayed } = this.props.document;
+    if (!displayed) {
+      return (
+        <div>
+          <p>There is no document to preview</p>
+        </div>
+      );
+    }
+
+    const url = assembleProjectUrl({ displayed, options });
+
+    if (!url) {
+      return (
+        <div>
+          <p>Hmm. Having problems constructing the web front-end URL.</p>
+        </div>
+      );
+    }
+
+    return (
+      <iframe
+        style={{ height: "100%", width: "100%" }}
+        src={url}
+        frameBorder={"0"}
+      />
+    );
+  }
+}
+
+export default IframePreview;
--- packages/studio/structure/pages.js
+++ packages/studio/structure/pages.js
@@ -1,5 +1,6 @@
 // import React from 'react';
 import S from "@sanity/desk-tool/structure-builder";
+import IframePreview from "../src/components/previews/iframe";
 // import Emoji from 'a11y-react-emoji'
 
 // const Icon = () => <Emoji style={{ fontSize: '2rem' }} symbol='📄' />
@@ -13,4 +14,18 @@ export const PageMenuItem = S.listItem()
       .menuItems(S.documentTypeList("page").getMenuItems())
       .filter("_type == $type")
       .params({ type: "page" })
+      .child(documentId =>
+        S.document()
+          .documentId(documentId)
+          .schemaType("page")
+          .views([
+            S.view.form(),
+            S.view
+              .component(IframePreview)
+              .options({
+                previewURL: "https://johnnymoses-com-1628856122.gtsb.io",
+              })
+              .title("Web Preview"),
+          ])
+      )
   );
