b8bd440f4801fb4c44b358df5469c778a4d5db92
Charlie Wright <charlie@cwgw.co>
2020-07-08 20:05:19 -0700

    Add calendar document

---
+++ packages/studio/schemas/types/calendar.js
@@ -0,0 +1,38 @@
+export default {
+  name: "calendar",
+  title: "Calendar",
+  type: "document",
+  liveEdit: false,
+  // You probably want to uncomment the next line once you've made the pages documents in the Studio. This will remove the pages document type from the create-menus.
+  // __experimental_actions: ['update', 'publish', /* 'create', 'delete' */],
+  fields: [
+    {
+      title: "Calendar ID",
+      name: "calendarId",
+      type: "string",
+    },
+    {
+      title: "Sync Token",
+      name: "nextSyncToken",
+      type: "string",
+      readOnly: true,
+    },
+    {
+      title: "Notification Channel ID",
+      name: "channelId",
+      type: "string",
+      readOnly: true,
+    },
+    {
+      title: "Notification Channel Expiration",
+      name: "channelExpiration",
+      type: "datetime",
+      readOnly: true,
+    }
+  ],
+  preview: {
+    select: {
+      title: "calendarId",
+    },
+  },
+};
