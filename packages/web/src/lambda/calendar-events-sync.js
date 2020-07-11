95664ad0695350cea0df9dcbf2659280eb99aea0
Charlie Wright <charlie@cwgw.co>
2020-07-10 21:41:45 -0700

    Test events webhook

--- packages/web/src/lambda/calendar-events-sync.js
+++ packages/web/src/lambda/calendar-events-sync.js
@@ -2,7 +2,7 @@ const { google } = require("googleapis");
 const sanityClient = require("@sanity/client");
 const crypto = require("crypto");
 
-const { decode, returnResponse } = require("./utils/request-config");
+const { decode, returnResponse, returnError } = require("./utils/request-config");
 
 const {
   GOOGLE_SERVICE_ACCT_KEY,
@@ -58,118 +58,111 @@ module.exports.handler = async event => {
   });
 
   if (resourceState === "sync") {
-    const response = await calendar.events.list({ calendarId });
-    if (response.status !== 200) {
-      console.error(
-        `Could not fetch events from Google Calendar ${calendarId}`
+    const response = await calendar.events
+      .list({ calendarId })
+      .catch(
+        returnError(`Could not fetch events from Google Calendar ${calendarId}`)
       );
-      return response;
+
+    if (response.status !== 200) {
+      console.log("Unexpected response. Expected status 204\n", response);
+    } else {
+      console.log("Successfully retrieved inital sync token");
     }
 
-    console.log(`Successfully retrieved inital sync token`);
     const { nextSyncToken } = response.data;
-    client
+
+    await client
       .transaction()
-      .createIfNotExists({
-        _id: documentId,
-        _type: "calendar",
-      })
+      .createIfNotExists({ _id: documentId, _type: "calendar" })
       .patch(documentId, p => p.set({ nextSyncToken }))
       .commit()
-      .then(() => {
-        console.log(`Successfully patched calendar '${documentId}' in Sanity`);
-        return returnResponse(200, "");
-      })
-      .catch(error => {
-        console.error("Sanity error:", error);
-        return returnResponse(500, {
-          error: "An internal server error has occurred",
-        });
-      });
+      .catch(returnError("Sanity error"));
+
+    console.log(`Successfully patched calendar '${documentId}' in Sanity`);
+    return returnResponse(200, "");
   }
 
   if (resourceState === "exists") {
-    try {
-      // fetch document for its sync token
-      const doc = await client.fetch(`*[_id==$id][0]`, { id: documentId });
-      if (!doc) {
-        // calendar is missing
-        // it may have been deleted after creating this notification channel
-      }
-      if (!doc.nextSyncToken) {
-        // this probably shouldn't happen
-      }
-      // fetch modified events
-      const res = await calendar.events.list({
+    // try {
+    // fetch document for its sync token
+    const doc = await client
+      .fetch(`*[_id==$id][0]`, { id: documentId })
+      .catch(
+        returnError(`Could not fetch calendar '${documentId}'`)
+      );
+
+    if (!doc) {
+      console.log(`Calendar '${documentId}' can't be retrieved. Maybe it's been deleted?`);
+      returnResponse(200, "");
+    }
+
+    // fetch modified events
+    const res = await calendar.events
+      .list({
         calendarId,
         syncToken: doc.nextSyncToken,
-      });
-      if (res.status !== 200 || res.data) {
-        // something went wrong
-      }
-      const { items, nextSyncToken } = res.data;
-      if (!items || items.length < 1) {
-        // no modified events
-        // this probably shouldn't happen either
-      }
-      const patchCalendar = client
-        .patch(documentId)
-        .set({ nextSyncToken })
-        .commit();
-      const patchEvents = items.map(item => {
-        const eventDocument = {
-          _type: "event",
-          _id: item.id,
-        };
-        const eventDocumentData = {
-          "content.main.title": item.summary,
-          "content.main.start": item.start.dateTime,
-          "content.main.end": item.end.dateTime,
-          "content.main.location": item.location,
-          "content.main.description": item.description,
-          "content.main.uid": item.iCalUID,
-          "content.main.created": item.created,
-          "content.main.updated": item.updated,
-          "content.main.htmlLink": item.htmlLink,
-        };
-        return client
-          .transaction()
-          .createIfNotExists(eventDocument)
-          .patch(item.id, p => p.set(eventDocumentData))
-          .commit();
-      });
+      })
+      .catch(
+        returnError(`Could not fetch events from Google Calendar ${calendarId}`)
+      );
 
-      await Promise.all([patchCalendar, ...patchEvents]);
-      return returnResponse(200, "");
-    } catch (error) {
-      console.error(error);
-      return returnResponse(500, {
-        error: "An internal server error has occurred",
-      });
+    if (res.status !== 200 || res.data) {
+      console.log("Unexpected response. Expected status 204\n", response);
+    } else {
+      console.log("Successfully retrieved modified events");
     }
+  
+    const { items, nextSyncToken } = res.data;
+
+    const patchCalendar = client
+      .patch(documentId)
+      .set({ nextSyncToken })
+      .commit()
+      .catch(
+        returnError(`Couldn't patch calendar ${documentId}`)
+      );
+
+    const patchEvents = items.map(item => {
+      if (!item.start.dateTime) {
+        // item is probably deleted
+        console.log("\n", { event }, "\n",);
+        return Promise.resolve()
+      }
+      
+      const eventDocument = {
+        _type: "event",
+        _id: item.id,
+      };
+      const eventDocumentData = {
+        "content.main.title": item.summary,
+        "content.main.start": item.start.dateTime,
+        "content.main.end": item.end.dateTime,
+        "content.main.location": item.location,
+        "content.main.description": item.description,
+        "content.main.uid": item.iCalUID,
+        "content.main.created": item.created,
+        "content.main.updated": item.updated,
+        "content.main.htmlLink": item.htmlLink,
+      };
+      return client
+        .transaction()
+        .createIfNotExists(eventDocument)
+        .patch(item.id, p => p.set(eventDocumentData))
+        .commit()
+        .catch(
+          returnError("Sanity error")
+        );
+    });
+
+    await Promise.all([patchCalendar, ...patchEvents]);
+    return returnResponse(200, "");
   }
 
+  if (resourceState === "not_exists") {
+    console.log("\n", "We got one! type is 'not_exists'", "\n");
+    console.log({ event });
+  }
+  
   return returnResponse(400, "");
 };
-
-// {
-//   kind: 'calendar#event',
-//   etag: '"3188151511896000"',
-//   id: '2qujrf9okv8f04nlac1atnm0cm',
-//   status: 'confirmed',
-//   htmlLink: 'https://www.google.com/calendar/event?eid=MnF1anJmOW9rdjhmMDRubGFjMWF0bm0wY20gam9obm55bW9zZXMuY29tQG0',
-//   created: '2020-07-06T22:49:15.000Z',
-//   updated: '2020-07-06T22:49:15.948Z',
-//   summary: 'AN EVENT',
-//   description: 'Spain this time! Look at that!',
-//   location: 'Barcelona, Spain',
-//   creator: [Object],
-//   organizer: [Object],
-//   start: [Object],
-//   end: [Object],
-//   iCalUID: '2qujrf9okv8f04nlac1atnm0cm@google.com',
-//   sequence: 0,
-//   guestsCanInviteOthers: false,
-//   guestsCanSeeOtherGuests: false,
-//   reminders: [Object]
-// },
